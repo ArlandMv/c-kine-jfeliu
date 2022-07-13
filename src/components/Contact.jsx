97fb3f196caa0e26b8d04040246526751806d927
ArlandMv <arlandmichelenav@gmail.com>
2022-07-12 20:07:35 -0400

    feat: added data-netlify-honeypot

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,10 +16,10 @@ const Contact = () => {
 					method="POST"
 					data-netlify="true"
 					onSubmit="submit"
-					/* data-netlify-honeypot="important" */
+					data-netlify-honeypot="important"
 					className="max-w-xl py-6 mx-auto">
-					{<input type="hidden" name="form-name" value="kine-contact" />}
-					{/* <input className="d-none" name="important" /> */}
+					<input type="hidden" name="form-name" value="kine-contact" />
+					<input className="d-none" name="important" />
 					<div className="mb-4">
 						<label for="name" className="text-gray-100">
 							Nombre
